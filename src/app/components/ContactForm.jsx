dd3f0b40ebdcc65ff696164bd9acc3767b882b39
vivek-chaprana <vivek2003ji@gmail.com>
2023-04-24 21:22:53 +0530

    Fix: Minor Error Fix @ ContactForm

--- src/app/components/ContactForm.jsx
+++ src/app/components/ContactForm.jsx
@@ -34,7 +34,7 @@ const ContactForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:3000/api/contact", {
+      const res = await fetch("/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
